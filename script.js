74ccfe561ba1499f204f939c5733d1f4b98023a8
Kim <ktla3223@gmail.com>
2023-09-05 16:13:26 -0700

    Removed extra space and replaced toggle with fadeOu()

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 console.log("Welcome to The Ultimate jQuery Course!🚀");
 
+// make sure our DOM is fully loaded
+$(document).ready(function() {
+// or
 /* $("#third").hide(); */
-
-/*
-$("#btn_hide").click(function() {
-    $(".second").hide();
+    $("#btn_hide").click(function() {
+        $(".second").fadeOut();
+    })
+    /* event.which shows what number of the button that was clicked*/
+    $("body").keydown(function(event) {
+        if(event.which === 72) {
+            $(".second").toggle(2000, function() {
+                console.log("Task Completed!");
+            })
+            // press H to hide it
+        }
+        if(event.which === 83) {
+            $(".second").show()
+            // press S to be visible
+        }
+    });
 })
-*/
-
-
-/* event.which shows what number of the button that was clicked*/
-
-$("body").keydown(function(event) {
-    if(event.which === 72) {
-        $(".second").toggle(2000, function() {
-            console.log("Task Completed!");
-        })
-        // press H to hide it
-    }
-    if(event.which === 83) {
-        $(".second").show()
-        // press S to be visible
-    }
-});
