b341087d1151dcec512821258962039a19d4fd7c
Kim <ktla3223@gmail.com>
2023-09-05 14:57:22 -0700

    Used event.which to determine the number of the button that was clicked

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ console.log("Welcome to The Ultimate jQuery Course!🚀");
 
 /* $("#third").hide(); */
 
+/*
 $("#btn_hide").click(function() {
     $(".second").hide();
+})
+*/
+
+$("body").keydown(function(event) {
+    console.log(event.which);
 })
