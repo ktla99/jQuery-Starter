46422f6c65cfadcaa3ccd858ea48f0b9125dcb18
Kim <ktla3223@gmail.com>
2023-09-07 11:35:58 -0700

    Put in 3 parameters and logged them to see what they output

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ console.log("Welcome to The Ultimate jQuery Course!🚀");
 
 // make sure our DOM is fully loaded
 $(document).ready(function() {
-    
+    $('#output').load("sample.txt", function(response,
+    statusTxt, xhr) {
+        console.log(response);
+        console.log(statusTxt);
+        console.log(xhr);
+    })
 });
