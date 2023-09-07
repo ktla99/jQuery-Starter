bc75e210dfe1bf5e08ae2d1def6fb334ec3ef11b
Kim <ktla3223@gmail.com>
2023-09-07 11:48:59 -0700

    Commented out previous work and get jsonplaceholder user information and logged its data and status

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ console.log("Welcome to The Ultimate jQuery Course!🚀");
 
 // make sure our DOM is fully loaded
 $(document).ready(function() {
-    $('#output').load("sample.txt", function(response,
+    /* $('#output').load("sample.txt", function(response,
     statusTxt, xhr) {
-        console.log(response);
-        console.log(statusTxt);
-        console.log(xhr);
-    })
-});
+        if (statusTxt === "success") alert("Success!!");
+        if (statusTxt === "error") alert("Error:" + xhr.statusText);
+        });
+        */
+       $.get("https://jsonplaceholder.typicode.com/users", 
+       function(data, status) {
+        console.log(data, status);
+       })
+    });
