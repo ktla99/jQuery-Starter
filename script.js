037c38c2c52fb890646c98b2f6fe6ff91938813e
Kim <ktla3223@gmail.com>
2023-09-07 11:27:52 -0700

    Reset js back to just document.ready

--- script.js
+++ script.js
@@ -2,20 +2,5 @@ console.log("Welcome to The Ultimate jQuery Course!🚀");
 
 // make sure our DOM is fully loaded
 $(document).ready(function() {
-    $('input').focus(function() {
-        $(this).css("background-color", "tomato");
-    })
     
-    $('input').blur(function() {
-        $(this).css("background-color", "");
-    })
-    
-    $('input').change(function() {
-        console.log($(this).val());
-    })
-
-    $("#registration_form").submit(function(event) {
-        event.preventDefault();
-        console.log("Form successfully submit!");
-    })
 });
