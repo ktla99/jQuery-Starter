c64abdc41b8b8980d25f975b7fda646c02f037b0
Kim <ktla3223@gmail.com>
2023-09-07 10:58:53 -0700

    Removed the click function and added in input.focu() with function to change the background color when clicked in input box

--- script.js
+++ script.js
@@ -2,21 +2,7 @@ console.log("Welcome to The Ultimate jQuery Course!🚀");
 
 // make sure our DOM is fully loaded
 $(document).ready(function() {
-// or
-/* $("#third").hide(); */
-    $("#btn_hide").click(function() {
-        let color = $("div.box").css("background-color");
-        $("#third").css({"color": color, "background-color": "black"});
-    });
-    /* event.which shows what number of the button that was clicked*/
-    $("body").keydown(function(event) {
-        if(event.which === 72) {
-            $(".second").hide();;
-            // press H to hide it
-        }
-        if(event.which === 83) {
-            $(".second").show()
-            // press S to be visible
-        }
-    });
+    $('input').focus(function() {
+        $(this).css("background-color", "tomato")
+    })
 });
